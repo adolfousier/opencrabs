4e49d83b036bf14a3bab434d3b3073b5cee8e3dd
Adolfo Usier <adolfousier@gmail.com>
2026-09-26 04:01:18 +0100

    test(acp): externalize catalog tests to src/tests, pin usage null-token cascade, cover --quiet parse (#1674)

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -25,6 +25,7 @@ pub mod a2a_notify_handler_test;
 pub mod a2a_server_test;
 pub mod a2a_session_notify_test;
 pub mod a2a_types_test;
+pub mod acp_catalog_test;
 pub mod acp_protocol_test;
 pub mod acp_replay_test;
 pub mod acp_transport_test;
